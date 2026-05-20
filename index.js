422f2bea5ff4de02f82310dda56b78aececba439
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-05-20 06:59:50 +0600

    fix origin

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
-        'https://pet-adoption-g53n.vercel.app/',
+        'https://pet-adoption-g53n.vercel.app',
     ],
     credentials: true 
 }));
